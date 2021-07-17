27cb9d08e24eb4b25fc173039d3d50a70767fdc5
irfianto <irfianto@localhost>
2021-07-17 15:45:04 +0700

    create mintOnly and mintAndSell for example

--- examples/mint.js
+++ examples/mint.js
@@ -3,51 +3,115 @@ const ParasSDK = require('../index')
 const fs = require('fs')
 const path = require('path')
 
-const main = async () => {
-	const parasSDK = new ParasSDK()
-
-	await parasSDK.init(
-		{
-			networkId: 'default',
-			nodeUrl: 'https://rpc.testnet.near.org',
-			walletUrl: 'https://wallet.testnet.near.org',
-			appName: 'Paras Testnet',
-			contractName: parasSDK.CONTRACT_DEV,
-			apiUrl: parasSDK.API_DEV,
-		},
-		{
-			isServer: true,
-		}
-	)
-
-	const cred = {
-		account_id: process.env.ROOT_ACCOUNT_ID,
-		public_key: process.env.ROOT_PUBLIC_KEY,
-		private_key: process.env.ROOT_PRIVATE_KEY,
+class MintApp {
+	constructor() {
+		this.parasSDK = new ParasSDK();
+		this.authToken = null;
+		this.accountId = null;
 	}
 
-	await parasSDK.login(cred)
-
-	try {
-		const authToken = await parasSDK.authToken()
-		const newToken = await parasSDK.mint(
+	async init() {
+		await this.parasSDK.init(
 			{
-				file: fs.createReadStream(path.join(__dirname, 'test.jpg')),
-				ownerId: cred.account_id,
-				supply: 5,
-				quantity: 0,
-				amount: 0,
-				name: 'Royalty X',
-				description: 'Key Description',
-				collection: 'Key',
-				royalty: 10
+				networkId: 'default',
+				nodeUrl: 'https://rpc.testnet.near.org',
+				walletUrl: 'https://wallet.testnet.near.org',
+				appName: 'Paras Testnet',
+				contractName: this.parasSDK.CONTRACT_DEV,
+				apiUrl: this.parasSDK.API_DEV,
 			},
-			authToken
+			{
+				isServer: true,
+			}
 		)
-		console.log(newToken)
-	} catch (err) {
-		console.log(err)
+		const cred = {
+			account_id: process.env.ROOT_ACCOUNT_ID,
+			public_key: process.env.ROOT_PUBLIC_KEY,
+			private_key: process.env.ROOT_PRIVATE_KEY,
+		}
+
+		await this.parasSDK.login(cred)
+		this.authToken = await this.parasSDK.authToken()
+		this.accountId = cred.account_id
 	}
+
+	async mintOnly(
+		fileName, 
+		supply, 
+		name, 
+		description, 
+		collection, 
+		royalty
+	) {
+		try {
+			const newToken = await this.parasSDK.mint(
+				{
+					file: fs.createReadStream(path.join(__dirname, fileName)),
+					ownerId: this.accountId,
+					supply: supply,
+					quantity: 0,
+					amount: 0,
+					name: name,
+					description: description,
+					collection: collection,
+					royalty: royalty
+				},
+				this.authToken
+			)
+			console.log(newToken)
+			return newToken
+		} catch (err) {
+			console.log(err)
+		}
+
+	}
+
+	async mintAndSell(
+		fileName, 
+		supply, 
+		name, 
+		description, 
+		collection, 
+		royalty,
+		quantity,
+		amount
+	) {
+		try {
+			const newToken = await this.parasSDK.mint(
+				{
+					file: fs.createReadStream(path.join(__dirname, fileName)),
+					ownerId: this.accountId,
+					supply: supply,
+					quantity: quantity,
+					amount: amount,
+					name: name,
+					description: description,
+					collection: collection,
+					royalty: royalty
+				},
+				this.authToken
+			)
+			console.log(newToken)
+			return newToken
+		} catch (err) {
+			console.log(err)
+		}
+
+	}
+
+
+
+
+}
+
+const main = async() => {
+	const mintApp = new MintApp()
+
+	await mintApp.init()
+
+	//mintApp.mintOnly('test.jpg', 10, 'Royalty X', 'Key Description', 'Key', 10)
+	mintApp.mintAndSell('test.jpg', 10, 'Royalty XI Sell', 'Key Sell', 'Key', 11, 10, '1000000000000000000000000')
+
 }
 
 main()
