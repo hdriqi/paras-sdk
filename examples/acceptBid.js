c75c23b2a3360df11009fd817e92bf890fbb76f8
adeyanuar <christianade9@gmail.com>
2021-07-31 17:35:51 +0700

    add bid

---
+++ examples/acceptBid.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+const ParasSDK = require('../index')
+const { parseNearAmount } = require('near-api-js/lib/utils/format')
+
+const main = async () => {
+	const parasSDK = new ParasSDK()
+
+	await parasSDK.init(
+		{
+			networkId: 'default',
+			nodeUrl: 'https://rpc.testnet.near.org',
+			walletUrl: 'https://wallet.testnet.near.org',
+			appName: 'Paras Testnet',
+			contractName: parasSDK.CONTRACT_DEV,
+			apiUrl: parasSDK.API_DEV,
+		},
+		{
+			isServer: true,
+		}
+	)
+
+    const cred = {
+        account_id: process.env.ROOT_ACCOUNT_ID,
+        public_key: process.env.ROOT_PUBLIC_KEY,
+        private_key: process.env.ROOT_PRIVATE_KEY,
+    }
+
+    await parasSDK.login(cred)
+    this.authToken = await parasSDK.authToken()
+
+	try {
+        const res = await parasSDK.acceptBid(
+            {
+                accountId: cred.account_id,
+                tokenId: "bafybeihivm3psgagxewyyj4osez5zygkyswojo22dbtzh3kplpvc6oscke",
+                quantity: 1,
+            }
+        )
+        console.log(res)
+        return res
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+main()
---
+++ examples/buy.js
@@ -0,0 +1,47 @@
+require('dotenv').config()
+const ParasSDK = require('../index')
+
+const main = async () => {
+	const parasSDK = new ParasSDK()
+
+	await parasSDK.init(
+		{
+			networkId: 'default',
+			nodeUrl: 'https://rpc.testnet.near.org',
+			walletUrl: 'https://wallet.testnet.near.org',
+			appName: 'Paras Testnet',
+			contractName: parasSDK.CONTRACT_DEV,
+			apiUrl: parasSDK.API_DEV,
+		},
+		{
+			isServer: true,
+		}
+	)
+
+    const cred = {
+		account_id: process.env.ROOT_ACCOUNT_ID,
+        public_key: process.env.ROOT_PUBLIC_KEY,
+        private_key: process.env.ROOT_PRIVATE_KEY,
+    }
+
+    await parasSDK.login(cred)
+    this.authToken = await parasSDK.authToken()
+    this.accountId = cred.account_id
+
+	try {
+		const res = await parasSDK.buy(
+			{
+				ownerId: this.accountId,
+				tokenId: "bafybeihyvmlz4yanombe2eb7aepcq77zr4dvbilzlagx66jotmdz34ngoe",
+				quantity: 1,
+				payment: 1,
+			}
+		)
+		console.log(res)
+		return res
+	} catch (err) {
+		console.log(err)
+	}
+}
+
+main()
